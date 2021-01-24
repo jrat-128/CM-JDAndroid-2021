33b92c5a52a01242e6c399a57ea26f97f955b890
jrat-128 <jrat@ua.pt>
2021-01-24 16:22:35 +0000

    Added camera functionality

--- TourLogAndroid/app/src/main/java/com/example/tourlogandroid/ui/camera/CameraFragment.java
+++ TourLogAndroid/app/src/main/java/com/example/tourlogandroid/ui/camera/CameraFragment.java
@@ -1,28 +1,49 @@
 package com.example.tourlogandroid.ui.camera;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.PermissionChecker;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.tourlogandroid.R;
 
+import static android.app.Activity.RESULT_OK;
+
 public class CameraFragment extends Fragment {
 
     private CameraViewModel cameraViewModel;
+
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
+
+    Button mCaptureBtn;
+    ImageView mImageView;
+    Uri image_uri;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         cameraViewModel =
                 new ViewModelProvider(this).get(CameraViewModel.class);
         View root = inflater.inflate(R.layout.fragment_camera, container, false);
+        // Textview for Fragment transition testing
         final TextView textView = root.findViewById(R.id.text_camera);
         cameraViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -30,6 +51,54 @@ public class CameraFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        mImageView = root.findViewById(R.id.image_view);
+        mCaptureBtn = root.findViewById(R.id.capture_image_btn);
+
+        //button click
+        mCaptureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
         return root;
+    }
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+        image_uri = getActivity().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        //Camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+    }
+
+    //handling permission result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //this method is called, when user presses Allow or Deny from Permission Request Popup
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if (grantResults.length > 0 && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED){
+                    //permission from popup was granted
+                    openCamera();
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //called when image was captured from camera
+
+        if (resultCode == RESULT_OK){
+            //set the image captured to our ImageView
+            mImageView.setImageURI(image_uri);
+        }
     }
 }
